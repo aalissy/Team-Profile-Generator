2da5bc86d381f9e777e30fcf17f79ec0da5e464d
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 16:28:48 -0500

    Updated Employee.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,29 @@
+const Employee = require("../library/Employee.js");
 
+describe("setName", () => {
+    it("should set a name from the text input", () => {
+    const e = new Employee("Lissy", 1, "lissygantos@gmail.com");
+    expect(e.name).toBe("Lissy");
+    });
+});
+
+describe("setID", () => {
+    it("should set an ID from the text input", () => {
+    const e = new Employee("Lissy", 1, "lissygantos@gmail.com");
+    expect(e.id).toBe(1);
+    });
+});
+
+describe("setEmail", () => {
+    it("should set an email from the text input", () => {
+    const e = new Employee("Lissy", 1, "lissygantos@gmail.com");
+    expect(e.email).toBe("lissygantos@gmail.com");
+    });
+})
+
+describe("getRole", () => {
+    it("gets the role from the selected role list", () => {
+    const e = new Employee("Lissy", 1, "lissygantos@gmail.com");
+    expect(e.getRole()).toBe("Employee");
+    });
+})
