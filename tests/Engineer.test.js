96a309973ebc8467899a7f62c282f83d11afb3f3
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 18:24:47 -0500

    added more engineer tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,14 +2,21 @@ const Engineer = require("../library/Engineer.js");
 
 describe("setGithub", () => {
     it("sets the github from the text input", () => {
-    const e = new Engineer("John", 3, "john@gmail.com", "johnson");
-    expect(e.github).toBe("johnson");
+        const e = new Engineer("John", 3, "john@gmail.com", "johnson");
+        expect(e.github).toBe("johnson");
     });
+})
+
+describe("getGithub", () => {
+    it("gets the inputted github", () => {
+        const e = new Engineer("Janna", 33, "janna@gmail.com", "paulson");
+        expect(e.getGithub()).toBe("paulson");
+    })
 })
 
 describe("getRole", () => {
     it("gets the role from the selected role list", () => {
-    const e = new Engineer("John", 3, "john@gmail.com", "johnson");
-    expect(e.getRole()).toBe("Engineer");
+        const e = new Engineer("John", 3, "john@gmail.com", "johnson");
+        expect(e.getRole()).toBe("Engineer");
     });
 })
