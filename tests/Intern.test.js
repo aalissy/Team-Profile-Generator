03e34ca2d0d4d8b4f08bf1539fcf273622164a44
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 18:25:13 -0500

    Added more intern tests

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -2,14 +2,21 @@ const Intern = require("../library/Intern.js");
 
 describe("setSchool", () => {
     it("sets the school from the text input", () => {
-    const i = new Intern("Jane", 4, "janey@gmail.com", "Loyola University of Chicago");
-    expect(i.school).toBe("Loyola University of Chicago");
+        const i = new Intern("Jane", 4, "janey@gmail.com", "Loyola University of Chicago");
+        expect(i.school).toBe("Loyola University of Chicago");
     });
 });
 
+describe("getSchool", () => {
+    it("gets the inputted school", () => {
+        const i = new Intern("Lewis", 210, "lewis@gmail.com", "University of Chicago");
+        expect(i.getSchool()).toBe("University of Chicago");
+    })
+})
+
 describe("getRole", () => {
     it("gets the role from the selected role list", () => {
-    const i = new Intern("Jane", 4, "janey@gmail.com", "Loyola University of Chicago");
-    expect(i.getRole()).toBe("Intern");
+        const i = new Intern("Jane", 4, "janey@gmail.com", "Loyola University of Chicago");
+        expect(i.getRole()).toBe("Intern");
     });
 })
