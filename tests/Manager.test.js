b270fffce37c4fdfb0742d4019a2df7f3308fc9b
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 18:25:36 -0500

    added more manager tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,7 +1,14 @@
 const Manager = require("../library/Manager.js");
 
+describe("setOfficeNumber", () => {
+    it("sets the office number from the text input", () => {
+        const m = new Manager("Delilah", 230, "delilah@gmail.com", 211);
+        expect(m.officeNumber).toBe(211);
+    })
+})
+
 describe("getOfficeNumber", () => {
-    it("gets the office number", () => {
+    it("gets the inputted office number", () => {
         const m = new Manager("Lily", 2, "lily@gmail.com", 44);
         expect(m.getOfficeNumber()).toBe(44);
     });
