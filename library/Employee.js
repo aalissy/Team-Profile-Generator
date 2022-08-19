689470e39f3578e826cb34b26b26e75845ecd34c
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 21:17:21 -0500

    Added comments

--- library/Employee.js
+++ library/Employee.js
@@ -1,21 +1,35 @@
+// Defines a class Employee
 class Employee {
+    // Constructor with name, id, and email as paramaters
     constructor(name, id, email) {
+        // Sets this.name equal to name
         this.name = name;
+        // Sets this.id equal to id
         this.id = id;
+        // Sets this.email equal to email
         this.email = email;
     }
+    // Defines a function getName()
     getName() {
+        // Return this.name
         return this.name;
     };
+    // Defines a function getID()
     getID() {
+        // Return this.id
         return this.id;
     };
+    // Defines a function getEmail()
     getEmail() {
+        // Return this.email
         return this.email;
     };
+    // Defines a function getRole()
     getRole() {
+        // Returns Employee
         return "Employee";
     };
 };
 
+// Exports Employee
 module.exports = Employee;
