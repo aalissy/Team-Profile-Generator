b531cdc9325e02f8808c42558e9fd4b395d8399b
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 21:18:18 -0500

    Added comments

--- library/Engineer.js
+++ library/Engineer.js
@@ -1,16 +1,26 @@
+// Defines a const Employee that requires the Employee.js in the library folder
 const Employee = require("./Employee.js");
 
+// Defines a class Engineeer that extends Employee
 class Engineer extends Employee {
+    // Constructor with name, id, email, and github as parameters
     constructor (name, id, email, github) {
+        // Super grabbing name, id, and email from employee
         super(name, id, email);
+        // Sets this.github equal to github
         this.github = github;
     };
+    // Defines a function getGithub()
     getGithub() {
+        // Returns this.github
         return this.github;
     }
+    // Defines a function getRole()
     getRole() {
+        // Returns Engineer
         return "Engineer";
     };
 };
 
+// Exports Engineer
 module.exports = Engineer;
