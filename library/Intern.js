28bc832f35fc22f4262f43e1223af6eb8ab52bf7
aalissy <32420245+aalissy@users.noreply.github.com>
2022-08-18 21:18:37 -0500

    Added comments

--- library/Intern.js
+++ library/Intern.js
@@ -1,16 +1,26 @@
+// Defines a const Employee that requires the Employee.js in the library folder
 const Employee = require("./Employee.js");
 
+// Defines a class Intern that extends Employee
 class Intern extends Employee {
+    // Constructor with name, id, email, and school as parameters
     constructor(name, id, email, school) {
+        // Super grabbing name, id, and email from employee
         super(name, id, email);
+        // Sets this.school equal to school
         this.school = school;
     };
+    // Defines a function getSchool()
     getSchool() {
+        // Returns this.school
         return this.school;
     };
+    // Defines a function getRole()
     getRole() {
+        // Returns Intern
         return "Intern";
     };
 };
 
+// Exports Intern
 module.exports = Intern;
